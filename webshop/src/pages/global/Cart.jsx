1818ee6e58b6c9c45553225abe30d8ff0a7f4b6e
rkangur <144773420+rkangur@users.noreply.github.com>
2023-11-26 07:30:04 +0200

    Kodused 6-8 (ostukorvi loogika)
    // 6. Ostukorvi lisamine HomePagest cart.json faili
    // 7. Cart lehe tegemine
    // 8. Filtreerimine HomePage lehel category järgi
    // näidake mitu toodet on avalehel nähtavad

--- webshop/src/pages/global/Cart.jsx
+++ webshop/src/pages/global/Cart.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react'
+import productsCartFile from '../../data/cart.json'
 
 function Cart() {
-  const [cart, setCart] = useState();
+  const [cart, setCart] = useState(productsCartFile);
   // 1. kuvage ostukorvi sisu Failist HTMLs välja
   // 2. võimaldage ostukorvist kustutada
   // 3. võimaldage ostukorvi tühjendada
@@ -10,15 +11,42 @@ function Cart() {
   // 6. Kui ostukorv on tühi, siis peitke osad kohad ära
   // 7. öelge, et ostukorv on tühi VÕI pange mingi pilt, et ostukorv on tühi
 
-  const removeFromCart = () => {}
+  const removeFromCart = (index) => {
+    productsCartFile.splice(index, 1);
+    setCart(productsCartFile.slice());
+  }
 
-  const emptyCart = () => {}
+  const emptyCart = () => {
+    productsCartFile.splice(0);
+    setCart(productsCartFile.slice());
+  }
 
-  const calculateCartSum = () => {}
+  const calculateCartSum = () => {
+    let sum = 0;
+    productsCartFile.forEach(product => {
+      sum += product.price;
+    })
+    return sum;
+  }
   
   return (
-    <div>Cart</div>
-  )
+    <div>
+      <div>Ostukorvis olevate toodete arv: {cart.length} </div>
+       {cart.map((product, index) => 
+      <div key={product.id}>
+        <img src={product.image} alt="" />
+        <div>{product.id}</div>
+        <div>{product.name}</div>
+        <div>{product.price}</div>
+        <button onClick={() => removeFromCart(index)}>X</button>
+      </div>)}
+        { cart.length === 0 ? <img className="chartImage" src="empty.png" alt="The cart is empty!" /> :
+         <div> 
+          <div>Kokku: {calculateCartSum()} € </div> 
+          <button onClick={() => emptyCart()}>Tühjenda ostukorv</button>
+        </div> }
+    </div>
+    )
 }
 
 export default Cart
--- webshop/src/pages/global/HomePage.jsx
+++ webshop/src/pages/global/HomePage.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import productsFromFile from '../../data/products.json'
-import { Link } from 'react-router-dom'
+import productsCartFile from '../../data/cart.json'
+import { Link } from 'react-router-dom';
 
 function HomePage() {
   const [products, setProducts] = useState(productsFromFile);
@@ -25,31 +26,47 @@ function HomePage() {
     setProducts(products.slice());
   }
 
-  const addToCart = () => {
-
+  const addToCart = (product) => {
+    productsCartFile.push(
+      {
+      "id": product.id,
+      "image": product.image,
+      "name": product.name,
+      "price": product.price,
+      "description": product.description,
+      "category": product.category,
+      "active": product.active
+    });
   }
 
-  const filterByFigure = (product) => {
-    const result = productsFromFile.filter(product.category === "Figure");
+  const filterByFigure = () => {
+    const result = productsFromFile.filter(product => product.category === "figure");
     setProducts(result);
   }
 
   const filterByLego = () => {
-    
+    const result = productsFromFile.filter(product => product.category === "lego");
+    setProducts(result);
   }
 
   const filterByStarWars = () => {
-    
+    const result = productsFromFile.filter(product => product.category === "star wars");
+    setProducts(result);
   }
 
   return (
     <div>
+      <div>Toodete arv: {products.length} tk</div>
+
       <button onClick={sortAZ}>Sorteeri kasvavalt</button>
       <button onClick={sortZA}>Sorteeri kahanevalt</button>
       <button onClick={sortPriceAsc}>Sorteeri hinna järgi kasvavalt</button>
       <button onClick={sortPriceDesc}>Sorteeri hinna järgi kahanevalt</button>
+      <button onClick={filterByFigure}>Filtreeri "Figure" tooted</button>
+      <button onClick={filterByLego}>Filtreeri "Lego" tooted</button>
+      <button onClick={filterByStarWars}>Filtreeri "StarWars" tooted</button>
 
-      {products.map(product => 
+      {products.map((product, index) => 
       <div key={product.id}>
         <img src={product.image} alt="" />
         <div>{product.id}</div>
@@ -61,6 +78,7 @@ function HomePage() {
         <Link to={"/product/" + product.id}>
           <button>Vaata lähemalt</button>
         </Link>
+        <button onClick={() => addToCart(product)}>Lisa ostukorvi</button>
       </div>)}
     </div>
   )
